a4b84501d8305d3040fa171fc51542f39d9098de
Josh Walles <countzeroasl@gmail.com>
2018-02-22 16:16:07 -0600

    beginning definition of InjectionMold

--- Lib/MoldClass.py
+++ Lib/MoldClass.py
@@ -1,2 +1,9 @@
-Adef foo():
+def foo():
     print("foo!")
+
+
+class InjectionMold:
+    def __init__(self, customer, cavities, partNumber):
+        self.customer = customer
+        self.cavities = cavities
+        self.partNumber = partNumber
