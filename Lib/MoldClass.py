be8dcb114a290fb8057f5e479d746345f58ed5f1
Josh Walles <countzeroasl@gmail.com>
2018-02-23 15:42:56 -0600

    Added comments and changed default values for moldWidth and moldHeight

--- Lib/MoldClass.py
+++ Lib/MoldClass.py
@@ -16,8 +16,13 @@ class InjectionMold:
         self.slideComplexity = slideComplexity
         self.lifterComplexity = lifterComplexity
 
-    def moldWidth(self, runnerSteel = 4, moldSteel = 4, slideSteel = 6):
+    def moldWidth(self, runnerSteel = 4, moldSteel = 4, slideSteel = 0):
+
+        """Returns the moldWidth in inches from the part height, steel needed for the runner system, outside mold support steel, and the steel for slides."""
         return ((partWidth*(cavities/2))+runnerSteel+moldSteel+slideSteel)
 
-    def moldHeight(self, runnerSteel = 4, moldSteel = 4, slideSteel = 6, spacingSteel = 2):
+    def moldHeight(self, moldSteel = 4, spacingSteel = 2, slideSteel = 0):
+
+        """Returns the moldHeight in inches from the part height, outside mold support steel, the steel in between cavities, and the steel for slides"""
+
         return ((partHeight*(cavities-(cavities/2)))+runnerSteel+moldSteel+slideSteel+(spacingSteel*((cavities-(cavities/2))-1)))
