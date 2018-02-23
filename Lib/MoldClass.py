8cb8aed19eee6a3b812b5a8112352a5711b8df29
Josh Walles <countzeroasl@gmail.com>
2018-02-23 15:48:44 -0600

    Formatting Changes

--- Lib/MoldClass.py
+++ Lib/MoldClass.py
@@ -9,6 +9,7 @@ class InjectionMold:
         self.partNumber = partNumber
         self.partWidth = partWidth
         self.partHeight = partHeight
+        self.partDepth = partDepth
         self.numberSlides = numberSlides
         self.numberLifters = numberLifters
         self.coreComplexity = coreComplexity
@@ -19,7 +20,7 @@ class InjectionMold:
     def moldWidth(self, runnerSteel = 4, moldSteel = 4, slideSteel = 0):
 
         """Returns the moldWidth in inches from the part height, steel needed for the runner system, outside mold support steel, and the steel for slides."""
-        
+
         return ((partWidth*(cavities/2))+runnerSteel+moldSteel+slideSteel)
 
     def moldHeight(self, moldSteel = 4, spacingSteel = 2, slideSteel = 0):
@@ -27,3 +28,4 @@ class InjectionMold:
         """Returns the moldHeight in inches from the part height, outside mold support steel, the steel in between cavities, and the steel for slides"""
 
         return ((partHeight*(cavities-(cavities/2)))+runnerSteel+moldSteel+slideSteel+(spacingSteel*((cavities-(cavities/2))-1)))
+
